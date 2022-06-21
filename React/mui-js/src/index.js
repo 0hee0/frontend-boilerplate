a6bde23a8b4e07ef07888e00736cd6b8f0a045c9
0hee0 <suhee0206@gmail.com>
2022-06-22 01:03:40 +0900

    feat: add login/signup with redux to React boilerplate

--- React/mui-js/src/index.js
+++ React/mui-js/src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import rootReducer from './services/reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
